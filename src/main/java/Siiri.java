e3a12235b990e9e89d5ab5df8c565802a6c1441a
Kaspar Papli <kaspar.papli@gmail.com>
2016-01-27 15:36:36 +0200

    Added initial method definitions and docs to classes Siiri and Syllabus

--- src/main/java/Siiri.java
+++ src/main/java/Siiri.java
@@ -1,6 +1,20 @@
-
+import java.util.List;
 
 public interface Siiri {
+
+
+    /**
+     *
+     * @param courseName
+     * @return a list of Courses <strong>partially</strong> matching <code>courseName</code>
+     */
+    List<Course> courseByName(String courseName);
+
+    /**
+     * @param courseCode
+     * @return a list of Courses <strong>partially</strong> matching <code>courseCode</code>
+     */
+    List<Course> courseByCode(String courseCode);
 
 
 }
--- src/main/java/Syllabus.java
+++ src/main/java/Syllabus.java
@@ -1,6 +1,50 @@
-
+import java.util.List;
 
 public interface Syllabus {
 
+    /**
+     * @return Course whose Syllabus this is
+     */
+    Course getCourse();
+
+    /**
+     * @return Semester on which this Syllabus takes place
+     */
+    Semester getSemester();
+
+    /**
+     * @return whether this Syllabus is for full-time study
+     */
+    Boolean isStationary();
+
+    /**
+     * @return number of attendants
+     */
+    Integer getAttendants();
+
+    /**
+     * @return list of teaching Lecturers
+     */
+    List<Lecturer> lecturers();
+
+    /**
+     * @return Language that is used for teaching
+     */
+    Language getLanguage();
+
+    /**
+     * @return whether this Syllabus's grading is differentiating
+     */
+    Boolean isDifferentiating();
+
+    /**
+     * @return minimum number of attendants required for it to take place
+     */
+    Integer minAttendants();
+
+    /**
+     * @return maximum number of attendants that are allowed to register
+     */
+    Integer maxAttendants();
 
 }
